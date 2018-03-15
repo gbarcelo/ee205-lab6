8311c5f5e080208da1c635f70f450ed098e30cd8
gbarcelo <gbarcelo@users.noreply.github.com>
2018-03-14 22:43:41 -1000

    initial algorithm for cook written

--- project/src/Cook.cpp
+++ project/src/Cook.cpp
@@ -1,1 +1,28 @@
 #include "Cook.hpp"
+
+Cook::Cook(SupplyRunner& runner_, Kitchen& kitchen_):
+        runner(runner_),
+        kitchen(kitchen_) { }
+
+void Cook::prepare_dish(
+        std::stack<Order>& orders,
+        std::queue<std::pair<std::size_t,
+        Dish>>& finished_dishes){
+  // pop order
+  Order o = orders.pop();
+  // read order
+  std::vector<std::string> orderIngreds = o.get_items();
+  // from recipes, get order ingredients
+
+  std::vector<Ingredient> runnersBag;
+  // supplyrunner get ingredients
+  runnersBag = runner.get_ingredients(
+  // (try/catch here?)
+
+  // some vector<ingredients> to IngredientMap conversion here
+
+  // kitchen order
+  Dish dish = kitchen.prepare_dish(runnersBag)
+  // queue order
+
+}
