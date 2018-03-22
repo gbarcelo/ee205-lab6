060a194deaf1885ff7ceaee6b24fad62795789d8
gbarcelo <gbarcelo@users.noreply.github.com>
2018-03-21 22:07:47 -1000

    record preliminary algorithm for cashier

--- project/src/Cashier.cpp
+++ project/src/Cashier.cpp
@@ -1,1 +1,20 @@
 #include "Cashier.hpp"
+
+Cashier::Cashier(double money_) : money(money_) { }
+
+double Cashier::get_money() const {
+  return money;
+}
+
+void Cashier::serve_customer(
+        std::queue<Customer>& line,
+        std::stack<Order>& orders){
+  // take customer off queue and assign them a unique ID
+  // if order is valid
+    // format order according to cashier.hpp
+  // calculate cost of order
+  // charge customer appropriate amount
+    // if customer doesn't have enough money, expel them instead
+  // take formatted order and convert to Order class (id, vector of items)
+  // push order to orders stack
+}
