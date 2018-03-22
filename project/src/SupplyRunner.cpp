0cdc3a2df599db86ee97b7358b0619813cb4ef22
gbarcelo <gbarcelo@users.noreply.github.com>
2018-03-22 06:05:26 -1000

    all tests pass, lab complete ready for submission

--- project/src/SupplyRunner.cpp
+++ project/src/SupplyRunner.cpp
@@ -6,9 +6,6 @@ std::vector<Ingredient> SupplyRunner::get_ingredients(IngredientMap ingredients)
   std::vector<Ingredient> runnersBag;
   // for every element in Ingredients
   for(IngredientMap::const_iterator i = ingredients.begin(); i != ingredients.end(); ++i){
-    // for every element in storeroom
-    // for (IngredientMap::const_iterator j = storeroom.begin(); j != storeroom.end(); ++j){
-      // if ingredient[i] != Storeroom.end()
     IngredientMap::iterator s = storeroom.find(i->first);
     if (s == storeroom.end()){throw "Ingredient not found";}
     if (s->second < i->second){throw "Not enough of this ingredient";}
@@ -16,13 +13,5 @@ std::vector<Ingredient> SupplyRunner::get_ingredients(IngredientMap ingredients)
     for(unsigned j = 0; j < i->second; ++j) {runnersBag.push_back(s->first);}
     //} // may not need nexted loop because .find() traverses map
   }
-
-
-        // remove ingredient,amount
-        // add to "supply runner's bag"
-      // else throw INGREDIENT NOT FOUND
-
-  //return "supply runner's bag" vector
-
   return runnersBag;
 }
