4231bd3580fb324458bd6b592904643273a6673e
gbarcelo <gbarcelo@users.noreply.github.com>
2018-03-22 05:48:38 -1000

    cashier completed

--- project/src/Cashier.cpp
+++ project/src/Cashier.cpp
@@ -1,7 +1,9 @@
 #include "Cashier.hpp"
+#include "Cook.hpp"
 
 // #include <vector>
 #include "restaurant-utility.hpp"
+
 
 Cashier::Cashier(double money_) : money(money_), uid_count(0) { }
 
@@ -13,7 +15,7 @@ void Cashier::serve_customer(
         std::queue<Customer>& line,
         std::stack<Order>& orders){
 
-  Customer currCustomer;
+  Customer *currCustomer;
   std::size_t currCustID;
   std::string numStr, singleItem;
   int quant;
@@ -21,22 +23,22 @@ void Cashier::serve_customer(
 
 
   // take customer off queue and assign them a unique ID
-  currCustomer = line.front();
+  currCustomer = &(line.front());
   currCustID = uid_count;
-  line.pop();
   uid_count++;
 
   // take customer order and tokenize
-  tokOrder = split(currCustomer.take_order());
+  tokOrder = split(currCustomer->take_order(),' ');
   // for every 2 tokens, get quantity of a single item in order
-  for(auto i = tokOrder.begin(); i != tokOrder.end(); i+=2){
-    quant = std::stoi(i);
-    singleItem = i+1;
+  for(unsigned int i = 0; i < tokOrder.size(); i+=2){
+    quant = std::stoi(tokOrder[i]);
+    singleItem = tokOrder[i+1];
 
     // if single item in order is not valid, expel customer
     auto dishCheck = recipes.find(singleItem);
     if (dishCheck==recipes.end()){
-      currCustomer.expel();
+      currCustomer->expel();
+      line.pop();
       return;
     } else {
       // else, format order to add to orders stack
@@ -49,29 +51,17 @@ void Cashier::serve_customer(
   // calculate cost of order
   double total = calculate_order_cost(currCustOrder);
   // if customer doesn't have enough money, expel them
-  if (currCustomer.get_money()<total){currCustomer.expel();}
+  if (currCustomer->get_money()<total){
+    currCustomer->expel();
+    line.pop();
+    return;
+  } else {
     // else charge customer appropriate amount
-    else{currCustomer.charge_money(total);}
+    money += currCustomer->charge_money(total);
+  }
 
   //push completed order to orders stack
-  orders.push(Order(uid_count,currCustOrder));
+  orders.push(Order(currCustID,currCustOrder));
   // cleanup
-
+  line.pop();
 }
-
-// vector<std::string> split(const char *str, char c = ' ')
-// {
-//     vector<std::string> result;
-//
-//     do
-//     {
-//         const char *begin = str;
-//
-//         while(*str != c && *str)
-//             str++;
-//
-//         result.push_back(string(begin, str));
-//     } while (0 != *str++);
-//
-//     return result;
-// }
